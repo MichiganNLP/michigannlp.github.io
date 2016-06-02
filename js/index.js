72280e5169083b668eb880cfc92b83d471eb275f
wenlaura <wenlaura@umich.edu>
2016-06-02 16:03:53 -0400

    Adding recent news to Girls Encoded

--- js/index.js
+++ js/index.js
@@ -3,8 +3,6 @@ var category; //the category of the page (ie. LIT, Girls Encoded)
 
 $(document).ready(function() {
     category = $('meta[name=category]').attr("content");
-    alert("category");
-    alert(category);
 
     //Dynamically load recent news
      $.ajax({
@@ -28,6 +26,10 @@ function processRecentNews(allText) {
         var news = {date:data[0], description:data[1], link:data[2], category:data[3]};
 
         var allCats = news.category.split(',');
+        alert("page category");
+        alert(category);
+        alert("allCats");
+        alert(allCats);
         var found = 0;
         //Is the document category in this array?
         for(var j=0; j<allCats.length; j++) {
@@ -40,6 +42,7 @@ function processRecentNews(allText) {
             }
         }
         if(found==1) {
+            alert("it's a match!");
             var entry = '<p class="recent-news-date">' + news.date + '</p><p class="lead">' + news.description;
             if(news.link) {
                 entry = entry + ' (see more <a href="' + news.link + '">here</a>)</p>';
