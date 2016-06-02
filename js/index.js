02a4acd9e3f74cb211badb97a6d2dee97c91eda4
wenlaura <wenlaura@umich.edu>
2016-06-02 13:37:59 -0400

    Updated recent news

--- js/index.js
+++ js/index.js
@@ -12,12 +12,11 @@ $(document).ready(function() {
 //<p class="recent-news-date">Sample date0</p>
 //<p class="lead">This is a description of what's happening0 (see more <a href="">here</a>)</p>
 function processRecentNews(allText) {
-    var allTextLines = allText.split(/\r\n|\n/);
-    var headers = allTextLines[0].split(',');
-    var lines = [];
+    arrData = parseCsv(allText);
     
-    for (var i=1; i<allTextLines.length; i++) {
-        var data = allTextLines[i].split(',');
+    var rowNum = -1;
+    for (var i=1; i<arrData.length; i++) {
+        var data = arrData[i];
 
         if(data.length==headers.length) {
             $('#recent_news').append('<p class="recent-news-date">' + data[1] + '</p>');
