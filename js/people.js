baaa224ce7c80a08c47247db1ca608e33e61cf31
wenlaura <wenlaura@umich.edu>
2016-07-18 14:32:51 -0400

    More

--- js/people.js
+++ js/people.js
@@ -24,15 +24,9 @@ function processPeople(allText) {
     arrData = parseCsv(allText);
     
     var rowNum = -1;
+    var entriesAdded = 0;
     for (var i=1; i<arrData.length; i++) {
         var data = arrData[i];
-        
-        if(i % 4 == 1) { //append new row
-            rowNum = rowNum + 1;
-            $('#people').append('<div class="row" id="row' + rowNum +'">');
-            alert("adding new row");
-            alert(i);
-        }
     
         var person = {image:data[0], name:data[1], description:data[2], uniqname:data[3], link:data[4], category:data[5]};
 
@@ -56,6 +50,13 @@ function processPeople(allText) {
                 entry = entry + person.name;
             }
             entry = entry + '</b></p>' + '<p class="lead">' + person.description + '</p><p class="lead">' + person.uniqname + ' at umich.edu</p>' + '</div>';
+            
+            entriesAdded = entriesAdded + 1;
+            if(entriesAdded % 4 == 1) { //append new row
+                rowNum = rowNum + 1;
+                $('#people').append('<div class="row" id="row' + rowNum +'">');
+            }
+            
             $('#row' + rowNum).append(entry);
         }
     }
