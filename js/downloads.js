f860d6a8b60377d16a8afd8aae5cf43a91f1f733
wenlaura <wenlaura@umich.edu>
2016-08-09 19:58:24 -0400

    More

--- js/downloads.js
+++ js/downloads.js
@@ -58,7 +58,7 @@ function processDownloads(allText,pageCategory) {
         categories[i].publications.sort();
         
         if(i==0) {
-            $('#downloads-outline').append('<p class="lead-nomargin"><b>'+categories[i].name+'</b></p>');
+            $('#downloads-outline').append('<p class="lead-nomargin"><a href="#' + categories[i].name + '"><b>'+categories[i].name+'</b></a></p>');
         } else {
             $('#downloads-outline').append('<p id="top-margin" class="lead-nomargin"><a href="#' + categories[i].name + '"><b>' +categories[i].name+'</b></a></p>');
         }
