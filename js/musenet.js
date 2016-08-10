24a15882a25a096b22a48978a5bd7820e3c757b9
wenlaura <wenlaura@umich.edu>
2016-08-10 13:57:10 -0400

    MuSeNet publications

--- js/musenet.js
+++ js/musenet.js
@@ -19,9 +19,12 @@ function processPublications(allText) {
         
         var publication = {title:data[1], authors:data[2], publication:data[3], link:data[4], category:data[5], demo:data[6], data:data[7], software:data[8], pageCategory:data[9], download:data[10], downloadName:data[11], downloadDescription:data[12], downloadLink:data[13], downloadDate:data[14], downloadLinkNames:data[15]};
         
-        if(publication.category=="MuSeNet") {
-            entry = showPublication(publication,true);
-            $('#musenet-publications').append(entry);
+        var allCats = publication.category.split(', ');
+        for(var j=0; j<allCats.length; ++j) {
+            if(allCats[j]=="MuSeNet") {
+                entry = showPublication(publication,true);
+                $('#musenet-publications').append(entry);
+            }
         }
     }
 }
