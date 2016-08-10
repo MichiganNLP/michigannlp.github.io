a5d3c162e07119444f5cd4eeec0400af7ebdb5f0
wenlaura <wenlaura@umich.edu>
2016-08-10 14:11:22 -0400

    Fixing publications page

--- js/publications.js
+++ js/publications.js
@@ -97,7 +97,7 @@ function processPublications(allText) {
                 entry = showPublication(publication,true);
                 $('#publications').append(entry);
             } else { //only show publication if it's in the proper category
-                var pubCategories = pubCategories.category.split(', ');
+                var pubCategories = publication.category.split(', ');
                 for(var k=0; k<pubCategories.length; ++k) {
                     if(pubCategories[k]==category) {
                         entry = showPublication(publication,true);
