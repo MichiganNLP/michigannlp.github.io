23a747507e879bdc766d9f0041a6a41d4a0d723c
wenlaura <wenlaura@umich.edu>
2016-08-10 13:16:12 -0400

    More

--- js/projects.js
+++ js/projects.js
@@ -72,7 +72,7 @@ function processProjects(allText,pageCategory) {
             
         entry = entry + '<div class="col-sm-6"><div class="panel panel-default"><div class="panel-heading"><h3 class="panel-title">' + past[i].name + '</h3></div><div class="panel-body"><p class="lead">' + past[i].description + '</p>';
         if(past[i].link) {
-            entry = entry + '<p class="lead">Please see more <a href="' + past[i].link + '" target="_blank">here</a></p>';
+            entry = entry + '<p class="lead">Please see more <a href="' + past[i].link + '" target="_blank">here</a>.</p>';
         }
         
         entry = entry + '</div></div></div>';
