67347c701e3156c5fa5fda49befc393cb7de2951
wenlaura <wenlaura@umich.edu>
2016-08-10 10:33:46 -0400

    Adding links to download page from publication page

--- js/showPublication.js
+++ js/showPublication.js
@@ -39,7 +39,7 @@ function showPublication(publication,includeDownloadLink) {
 
     if(includeDownloadLink && publication.download=="TRUE") {
         entry = entry + '<p class="lead-slim-indent"><em>' + publication.publication + '</em></p>';
-        entry = entry + '<p class="lead-slim-bottom">*More detailed resources for this paper can be found on the <a href="downloads.html#' + publication.category + '">downloads</a> page.</p>';
+        entry = entry + '<p class="lead-slim-bottom">**More detailed resources for this paper can be found on the <a href="downloads.html#' + publication.category + '">downloads</a> page.</p>';
     } else {
         entry = entry + '<p class="lead-slim-indent-bottom"><em>' + publication.publication + '</em></p>';
     }
