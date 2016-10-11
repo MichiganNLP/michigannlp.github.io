8b04d0a383a86b64462eaa2e70cf1bcba70f604e
wenlaura <wenlaura@umich.edu>
2016-10-11 11:58:44 -0400

    Adding alumni to people page

--- js/processPeople.js
+++ js/processPeople.js
@@ -8,12 +8,13 @@
 function processPeople(allText,pageCategory) {
     arrData = parseCsv(allText);
     
-    var rowNum = -1;
-    var entriesAdded = 0;
+    current = [];
+    alumni = [];
+    
     for (var i=1; i<arrData.length; i++) {
         var data = arrData[i];
     
-        var person = {image:data[0], name:data[1], description:data[2], uniqname:data[3], email_domain:data[4], link:data[5], category:data[6]};
+        var person = {image:data[0], name:data[1], description:data[2], uniqname:data[3], email_domain:data[4], link:data[5], alumni:data[6], category:data[7]};
 
         var allCats = person.category.split(',');
         var found = 0;
@@ -28,25 +29,61 @@ function processPeople(allText,pageCategory) {
             }
         }
         if(found==1) {
-            if(person.image) {
-                entry = '<div class="col-md-2"><img src="images/people/'+ person.image + '" class="profile_pic" alt="' + person.name + '"><p class="lead"><b>';
+            if(person.alumni) {
+                alumni.push(person);
             } else {
-                entry = '<div class="col-md-2"><img src="images/people/none.png" class="profile_pic" alt="No image available"><p class="lead"><b>';
+                current.push(person);
             }
-            if(person.link) {
-                entry = entry + '<a href="' + person.link + '">' + person.name + '</a>';
-            } else {
-                entry = entry + person.name;
-            }
-            entry = entry + '</b></p>' + '<p class="lead">' + person.description + '</p><p class="lead">' + person.uniqname + ' at ' + person.email_domain + '</p>' + '</div>';
-            
-            entriesAdded = entriesAdded + 1;
-            if(entriesAdded % 6 == 1) { //append new row
-                rowNum = rowNum + 1;
-                $('#people').append('<div class="row" id="row' + rowNum +'">');
-            }
-            
-            $('#row' + rowNum).append(entry);
         }
+    }
+        
+    var rowNum = -1;
+    var entriesAdded = 0;
+    for(var i=0; i<current.length; ++i) {
+        person = current[i];
+        if(person.image) {
+            entry = '<div class="col-md-2"><img src="images/people/'+ person.image + '" class="profile_pic" alt="' + person.name + '"><p class="lead"><b>';
+        } else {
+            entry = '<div class="col-md-2"><img src="images/people/none.png" class="profile_pic" alt="No image available"><p class="lead"><b>';
+        }
+        if(person.link) {
+            entry = entry + '<a href="' + person.link + '">' + person.name + '</a>';
+        } else {
+            entry = entry + person.name;
+        }
+        entry = entry + '</b></p>' + '<p class="lead">' + person.description + '</p><p class="lead">' + person.uniqname + ' at ' + person.email_domain + '</p>' + '</div>';
+        
+        entriesAdded = entriesAdded + 1;
+        if(entriesAdded % 6 == 1) { //append new row
+            rowNum = rowNum + 1;
+            $('#current').append('<div class="row" id="row' + rowNum +'">');
+        }
+        
+        $('#row' + rowNum).append(entry);
+    }
+    
+    var rowNum = -1;
+    var entriesAdded = 0;
+    for(var i=0; i<alumni.length; ++i) {
+        person = alumni[i];
+        if(person.image) {
+            entry = '<div class="col-md-2"><img src="images/people/'+ person.image + '" class="profile_pic" alt="' + person.name + '"><p class="lead"><b>';
+        } else {
+            entry = '<div class="col-md-2"><img src="images/people/none.png" class="profile_pic" alt="No image available"><p class="lead"><b>';
+        }
+        if(person.link) {
+            entry = entry + '<a href="' + person.link + '">' + person.name + '</a>';
+        } else {
+            entry = entry + person.name;
+        }
+        entry = entry + '</b></p>' + '<p class="lead">' + person.description + '</p><p class="lead">' + person.uniqname + ' at ' + person.email_domain + '</p>' + '</div>';
+        
+        entriesAdded = entriesAdded + 1;
+        if(entriesAdded % 6 == 1) { //append new row
+            rowNum = rowNum + 1;
+            $('#alumni').append('<div class="row" id="row' + rowNum +'">');
+        }
+        
+        $('#row' + rowNum).append(entry);
     }
 }
