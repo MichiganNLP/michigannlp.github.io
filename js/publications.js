2d97fd606ee0918fc817edb1638572a564ed0660
wenlaura <wenlaura@umich.edu>
2016-10-20 17:32:11 -0400

    Adding abstracts and bibtex to publication page

--- js/publications.js
+++ js/publications.js
@@ -29,7 +29,7 @@ function processPublications(allText) {
     for (var i=1; i<arrData.length; i++) {
         var data = arrData[i];
         
-        var publication = {id:i; title:data[1], authors:data[2], publication:data[3], link:data[4], category:data[5], demo:data[6], data:data[7], software:data[8], bibtex:data[9], abstract:data[10], pageCategory:data[11], download:data[12], downloadName:data[13], downloadDescription:data[14], downloadLink:data[15], downloadDate:data[16], downloadLinkNames:data[17]};
+        var publication = {id:i, title:data[1], authors:data[2], publication:data[3], link:data[4], category:data[5], demo:data[6], data:data[7], software:data[8], bibtex:data[9], abstract:data[10], pageCategory:data[11], download:data[12], downloadName:data[13], downloadDescription:data[14], downloadLink:data[15], downloadDate:data[16], downloadLinkNames:data[17]};
                 
         var allCats = publication.pageCategory.split(',');
         var found = 0;
