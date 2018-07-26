9d5cecdfe32d4eb782dcbf762850dc768ffc89a4
Laura Wendlandt <wenlaura@umich.edu>
2018-07-26 13:36:48 -0400

    Include presentation info on poster page

--- js/posters.js
+++ js/posters.js
@@ -13,17 +13,11 @@ $(document).ready(function() {
 function processPublications(allText) {
 	arrData = parseCsv(allText);
 
-	var rowNum = -1;
-	var entriesAdded = 0;
 	posters = []
 	for (var i=1; i<arrData.length; i++) {
 		var data = arrData[i];
 
 		var publication = {id:i, title:data[1], authors:data[2], publication:data[3], link:data[4], category:data[5], demo:data[6], data:data[7], software:data[8], bibtex:data[9], abstract:data[10], poster:data[11], posterTitle:data[12], posterConference:data[13], posterYear:data[14], posterLocation:data[15], pageCategory:data[16], download:data[17], downloadName:data[18], downloadDescription:data[19], downloadLink:data[20], downloadDate:data[21], downloadLinkNames:data[22]};
-
-		if(!publication.poster) {
-			continue;
-		}
 
 		var allCats = publication.pageCategory.split(',');
 		var found = 0;
@@ -37,11 +31,13 @@ function processPublications(allText) {
 				break;
 			}
 		}
-		if(found==1 && publication.poster) {
+		if(publication.poster) {
 			posters.push(publication)
 		}
 	}
 
+	var rowNum = -1;
+	var entriesAdded = 0;
 	for (var i=1; i<posters.length; i++) {
 		publication = posters[i]
 
