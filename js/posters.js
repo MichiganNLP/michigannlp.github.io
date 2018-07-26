72771ddee719eb8b9951c97074da123051281457
Laura Wendlandt <wenlaura@umich.edu>
2018-07-26 13:31:09 -0400

    Include presentation info on poster page

--- js/posters.js
+++ js/posters.js
@@ -15,6 +15,7 @@ function processPublications(allText) {
 
 	var rowNum = -1;
 	var entriesAdded = 0;
+	posters = []
 	for (var i=1; i<arrData.length; i++) {
 		var data = arrData[i];
 
@@ -36,43 +37,52 @@ function processPublications(allText) {
 				break;
 			}
 		}
-
 		if(found==1 && publication.poster) {
-			entry = '<div class="col-md-4"><a href="posters/'+publication.poster+'.pdf" target="_blank"><img src="posters/'+publication.poster+'.jpg" class="poster_image"></a><p class="lead centered">'+publication.posterTitle
-		
-			if(publication.link) {
-				entry = entry + ' (<a href="'+publication.link+'" target="_blank">full paper</a>)'
-			}
-
-			entry = entry + '</p>'
-			
-			if(publication.posterConference || publication.posterYear || publication.posterLocation) {
-				entry = entry + '<p class="lead centered"><i>Presented '
-				if(publication.posterConference) {
-					entry = entry + 'at ' + publication.posterConference
-					if(publication.posterYear || publication.posterLocation) {
-						entry = entry + ' '
-					}
-				}
-				if(publication.posterYear) {
-					entry = entry + 'in ' + publication.posterYear
-					if(publication.posterLocation) {
-						entry = entry + ' '
-					}
-				}
-				if(publication.posterLocation) {
-					entry = entry + 'in ' + publication.posterLocation
-				}
-				entry = entry + '</i></p>'
-			}
-			
-			entry = entry+'</div>'
-			entriesAdded = entriesAdded+1;
-			if (entriesAdded % 3 == 1) { //append new row
-				rowNum = rowNum+1;
-				$('#posters').append('<div class="row poster_row" id="row'+rowNum+'">');
-			}
-			$('#row'+rowNum).append(entry);
+			posters.append(publication)
 		}
+	}
+
+	for (var i=1; i<posters.length; i++) {
+		publication = posters[i]
+
+		entry = '<div class="col-md-4"><a href="posters/'+publication.poster+'.pdf" target="_blank"><img src="posters/'+publication.poster+'.jpg" class="poster_image"></a><p class="lead centered">'+publication.posterTitle
+	
+		if(publication.link) {
+			entry = entry + ' (<a href="'+publication.link+'" target="_blank">full paper</a>)'
+		}
+
+		entry = entry + '</p>'
+		
+		if(publication.posterConference || publication.posterYear || publication.posterLocation) {
+			entry = entry + '<p class="lead centered"><i>Presented '
+			if(publication.posterConference) {
+				entry = entry + 'at ' + publication.posterConference
+				if(publication.posterYear || publication.posterLocation) {
+					entry = entry + ' '
+				}
+			}
+			if(publication.posterYear) {
+				entry = entry + 'in ' + publication.posterYear
+				if(publication.posterLocation) {
+					entry = entry + ' '
+				}
+			}
+			if(publication.posterLocation) {
+				entry = entry + 'in ' + publication.posterLocation
+			}
+			entry = entry + '</i></p>'
+		}
+		
+		entry = entry+'</div>'
+		entriesAdded = entriesAdded+1;
+		if (entriesAdded % 3 == 1) { //append new row
+			rowNum = rowNum+1;
+			if((len(posters)/3)-1 > rowNum) { //not last row
+				$('#posters').append('<div class="row poster_row" id="row'+rowNum+'">');
+			} else { //last row
+				$('#posters').append('<div class="row" id="row'+rowNum+'">');
+			}
+		}
+		$('#row'+rowNum).append(entry);
 	}
 }
