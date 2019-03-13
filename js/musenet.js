cb2bd4a52ee04d473a32454e976a0aa5ce734c83
Laura Wendlandt <wenlaura@umich.edu>
2019-03-13 13:13:17 -0400

    Adding authors to poster page

--- js/musenet.js
+++ js/musenet.js
@@ -17,7 +17,7 @@ function processPublications(allText) {
     for (var i=1; i<arrData.length; i++) {
         var data = arrData[i];
         
-		var publication = {id:i, title:data[1], authors:data[2], publication:data[3], link:data[4], category:data[5], demo:data[6], data:data[7], software:data[8], bibtex:data[9], abstract:data[10], poster:data[11], posterTitle:data[12], posterConference:data[13], posterYear:data[14], posterLocation:data[15], pageCategory:data[16], download:data[17], downloadName:data[18], downloadDescription:data[19], downloadLink:data[20], downloadDate:data[21], downloadLinkNames:data[22]};
+		var publication = {id:i, title:data[1], authors:data[2], publication:data[3], link:data[4], category:data[5], demo:data[6], data:data[7], software:data[8], bibtex:data[9], abstract:data[10], poster:data[11], posterTitle:data[12],posterAuthors:data[13], posterConference:data[14], posterYear:data[15], posterLocation:data[16], pageCategory:data[17], download:data[18], downloadName:data[19], downloadDescription:data[20], downloadLink:data[21], downloadDate:data[22], downloadLinkNames:data[23], new:data[24], notes:data[25]};
         
         var allCats = publication.category.split(', ');
         for(var j=0; j<allCats.length; ++j) {
--- js/posters.js
+++ js/posters.js
@@ -17,7 +17,7 @@ function processPublications(allText) {
 	for (var i=1; i<arrData.length; i++) {
 		var data = arrData[i];
 
-		var publication = {id:i, title:data[1], authors:data[2], publication:data[3], link:data[4], category:data[5], demo:data[6], data:data[7], software:data[8], bibtex:data[9], abstract:data[10], poster:data[11], posterTitle:data[12], posterConference:data[13], posterYear:data[14], posterLocation:data[15], pageCategory:data[16], download:data[17], downloadName:data[18], downloadDescription:data[19], downloadLink:data[20], downloadDate:data[21], downloadLinkNames:data[22]};
+		var publication = {id:i, title:data[1], authors:data[2], publication:data[3], link:data[4], category:data[5], demo:data[6], data:data[7], software:data[8], bibtex:data[9], abstract:data[10], poster:data[11], posterTitle:data[12],posterAuthors:data[13], posterConference:data[14], posterYear:data[15], posterLocation:data[16], pageCategory:data[17], download:data[18], downloadName:data[19], downloadDescription:data[20], downloadLink:data[21], downloadDate:data[22], downloadLinkNames:data[23], new:data[24], notes:data[25]};
 
 		var allCats = publication.pageCategory.split(',');
 		var found = 0;
@@ -41,13 +41,17 @@ function processPublications(allText) {
 	for (var i=0; i<posters.length; i++) {
 		publication = posters[i]
 
-		entry = '<div class="col-md-4"><a href="posters/'+publication.poster+'.pdf" target="_blank"><img src="posters/'+publication.poster+'.jpg" class="poster_image"></a><p class="lead centered">'+publication.posterTitle
+		entry = '<div class="col-md-4"><a href="posters/'+publication.poster+'.pdf" target="_blank"><img src="posters/'+publication.poster+'.jpg" class="poster_image"></a><p class="lead centered"><b>'+publication.posterTitle+'</b>'
 	
 		if(publication.link) {
 			entry = entry + ' (<a href="'+publication.link+'" target="_blank">full paper</a>)'
 		}
 
 		entry = entry + '</p>'
+
+		if(publication.posterAuthors) {
+			entry = entry + '<p class="lead centered">'+publication.posterAuthors'</p>'
+		}
 		
 		if(publication.posterConference || publication.posterYear || publication.posterLocation) {
 			entry = entry + '<p class="lead centered"><i>Presented '
