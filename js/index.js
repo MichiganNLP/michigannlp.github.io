3cbbe226fc6c62a17a9d896199ea0fbb1ff2789a
Laura Biester <laurajpb@gmail.com>
2021-02-25 16:21:59 -0500

    Add zoom photo and change how photos on the front page are loaded

--- js/index.js
+++ js/index.js
@@ -8,6 +8,14 @@ $(document).ready(function() {
         dataType: "text",
         success: function(data) {processRecentNews(data,pageCategory);}
     });
+
+    //Dynamically load slideshow
+    $.ajax({
+        type: "GET",
+        url: "data/slideshow.csv",
+        dataType: "text",
+        success: function(data) {processSlideshow(data);}
+    });
 });
 
 //Template:
@@ -15,13 +23,17 @@ $(document).ready(function() {
 //<p class="lead">This is a description of what's happening0 (see more <a href="">here</a>)</p>
 function processRecentNews(allText,pageCategory) {
     arrData = parseCsv(allText);
-    
+
     var appended = 0; //Only append the first four recent events
-    var last = false;   
- 
+    var last = false;
+
     for (var i=1; i<arrData.length; i++) {
         var data = arrData[i];
-        
+
+        if (data.length == 1) {
+            continue;
+        }
+
         var news = {date:data[0], description:data[1], link:data[2], category:data[3]};
 
         var allCats = news.category.split(',');
@@ -42,7 +54,7 @@ function processRecentNews(allText,pageCategory) {
                 entry = entry + ' (<a href="' + news.link + '" target="_blank">link</a>)</p>';
             }
             entry = entry + '</p>';
-        
+
             if(appended < 8) { //Only show the most recent entries on the home page
                 $('#recent_news').append(entry);
                 appended = appended + 1;
@@ -61,4 +73,38 @@ function processRecentNews(allText,pageCategory) {
     	    }
 	}
     }
+}
+
+function processSlideshow(allText) {
+    arrData = parseCsv(allText);
+
+    sortedArr = arrData.slice(1).sort(function (a, b) {
+        return parseInt(a[2]) > parseInt(b[2]) ? 1 : -1});
+
+    var dataAdded = 0;
+    var slideshow = '';
+    for (var i=0; i<sortedArr.length; i++) {
+        var data = sortedArr[i];
+
+        if (data.length != 3 || data[2].length == 0) {
+            continue;
+        }
+
+        var image = {path:data[0], alt:data[1], position:parseInt(data[2])};
+
+        if (image.position === 0) {
+            slideshow += '<div class="item active"><img src="images/slideshow/' + image.path + '" alt="' + image.alt + '"></div>\n';
+        } else {
+            slideshow += '<div class="item"><img src="images/slideshow/' + image.path + '" alt="' + image.alt + '"></div>\n';
+        }
+        dataAdded++;
+    }
+
+    var indicators = '<li data-target="#myCarousel" data-slide-to="0" class="active"></li>';
+    for (var i=1; i<dataAdded; i++) {
+        indicators += '<li data-target="#myCarousel" data-slide-to=' + i.toString() + '></li>';
+    }
+
+    $('.carousel-indicators').append(indicators);
+    $('.carousel-inner').append(slideshow);
 }
