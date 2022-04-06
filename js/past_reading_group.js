6dc82c6af596ae80d2982a4f977d36dff436d466
Laura Biester <laurajpb@gmail.com>
2022-04-06 12:59:47 -0400

    Fix previous reading groups

--- js/past_reading_group.js
+++ js/past_reading_group.js
@@ -77,6 +77,7 @@ function processReadingGroup(allText,pageCategory) {
         $('#past-reading-group').append('<h2 class="featurette-heading">' + semesters[i].name + '</h2>');
         var entry = '<table id="reading-group-table"><tbody id="reading-group-tbody"><tr><th id="col1" class><p class="lead"><b>Date / Time</b></p></th><th id="col2" class="centered"><p class="lead"><b>Facilitator</b></p></th><th id="col3" class="centered"><p class="lead"><b>Paper</b></p></th><th id="col4" class="centered"><p class="lead"><b>Location</b></p></th></tr>'
 
+        var prevDisplayed = false;
         for(var j=0; j<semesters[i].events.length; ++j) {
             event = semesters[i].events[j];
             var liveOK = event.liveOK == "TRUE";
